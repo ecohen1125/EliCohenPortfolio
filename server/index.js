85c7114a406f41467ae53777d31af0a34a7ad774
ecohen1125 <ecohen1125@gmail.com>
2024-05-11 14:29:52 -0500

    changed origin in server

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(
     {
-        origin: ["https://deploy-mern-frontend.vercel.app"],
+        origin: ["https://eli-cohen-portfolio-api.vercel.app"],
         methods: ["POST", "GET"],
         credentials: true
     }
