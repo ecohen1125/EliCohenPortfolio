1f346cfbcf87660f2cb83d3d480b9e9ddf687f85
ecohen1125 <77858375+ecohen1125@users.noreply.github.com>
2024-05-11 14:53:47 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(
     {
-        origin: ["https://eli-cohen-portfolio-api.vercel.app"],
+        origin: ["https://eli-cohen-portfolio-frontend.vercel.app/"],
         methods: ["POST", "GET"],
         credentials: true
     }
